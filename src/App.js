c17fce944650622b33e618673f7b91d6b2b6e28b
ChakkaphatChanamee <s6703052411163@email.kmutnb.ac.th>
2026-01-08 10:25:32 +0700

    Problems with Event Hander
    Problems with Event Hander with using this in a Class
    Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { Content, Footer, Header } from "./func-components";
-import getDate from './class-components'
-import Calender from "./class-components";
+import Button from "./class-components";
 
 
 function App() {
-  return(
-    <>
-      <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+  return<Button/>
     
   }
 
