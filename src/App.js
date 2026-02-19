0ba4bf353ec7264f97b410a2626e49df3420f8eb
ChakkaphatChanamee <s6703052411163@email.kmutnb.ac.th>
2026-02-19 11:50:12 +0700

    Create Input type text password and textarea
    Create Input type text password and textarea

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import Router2 from "./router2";
-import Router3 from "./router3";
+import React from "react"
+import FormText from "./form-text"
 
 
-function App() {
-  return <Router3/>
-}
-
-export default App;
+export default function App() {
+  return <FormText/>
+  }
